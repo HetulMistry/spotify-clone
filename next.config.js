0b0fff3dd93578a8e3e22fc4fc5e3db43ff4838d
Hetul Mistry <Manishcmdgm812@gmail.com>
2023-11-19 18:18:06 +0530

    next.congif.js configs fixed.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["misc.scdn.co","i.scdn.co","geo-media.beatsource.com","i1.sndcdn.com","media.pitchfork.com","seed-mix-image.spotifycdn.com","zffxnbvszvviejpokuho.supabase.co"]
+    domains: ["misc.scdn.co","i.scdn.co","geo-media.beatsource.com","i1.sndcdn.com","media.pitchfork.com","seed-mix-image.spotifycdn.com","zffxnbvszvviejpokuho.supabase.co"]
   }
 };
 
